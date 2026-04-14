9706ef0f31d43c2d1f5072f97bb3912f106646f9
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-13 22:40:04 -0300

    refactor: removed incomes_dbt from /

--- incomes_dbt/tests/assert_agi_stub_between_1_and_6.sql
+++
@@ -1,3 +0,0 @@
-SELECT 1
-FROM {{ ref('stg_tax_data') }}
- WHERE NOT agi_stub BETWEEN 1 AND 6
--- incomes_dbt/tests/assert_tax_numbers_are_greater_than_zero.sql
+++
@@ -1,3 +0,0 @@
-SELECT 1
-FROM {{ ref('stg_tax_data') }}
-WHERE num_returns < 0 OR total_income < 0 OR total_tax < 0
