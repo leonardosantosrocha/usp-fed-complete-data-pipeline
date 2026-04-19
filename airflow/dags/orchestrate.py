bf20e41dc12eaca68ae91374039e19f0c2e1425d
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-19 16:23:58 -0300

    feat: add dbt tasks

--- airflow/dags/orchestrate.py
+++ airflow/dags/orchestrate.py
@@ -1,58 +1,14 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
 from datetime import datetime
-import os
-import time
-import psycopg2
-
-
-def wait_for_postgres(
-    host=os.getenv("POSTGRES_HOST", "postgres-app"),
-    port=int(os.getenv("POSTGRES_PORT", 5432)),
-    database=os.getenv("POSTGRES_DB", "incomes_db"),
-    user=os.getenv("POSTGRES_USER", "admin"),
-    password=os.getenv("POSTGRES_PASSWORD", "admin"),
-    timeout=60,
-    interval=3,
-):
-    start_time = time.time()
-
-    while True:
-        try:
-            conn = psycopg2.connect(
-                host=host,
-                port=port,
-                dbname=database,
-                user=user,
-                password=password,
-            )
-            conn.close()
-            print("PostgreSQL disponível")
-            return
-        except Exception as e:
-            if time.time() - start_time > timeout:
-                raise Exception(f"Timeout ao conectar no PostgreSQL: {e}")
-            print("Aguardando PostgreSQL...")
-            time.sleep(interval)
+from tasks import wait_db_task, dbt_tasks, ingestion_task
 
 
 with DAG(
-    dag_id="orchestrate_ingestion",
+    dag_id="lab_final",
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
     tags=["ingestion"],
 ) as dag:
 
-    wait_db = PythonOperator(
-        task_id="wait_for_postgres",
-        python_callable=wait_for_postgres,
-    )
-
-    ingestion_task = BashOperator(
-        task_id="run_ingestion",
-        bash_command="python /opt/app/src/ingestion/ingestion.py",
-    )
-
-    wait_db >> ingestion_task
+    wait_db_task() >> ingestion_task() >> dbt_tasks()
---
+++ airflow/dags/tasks.py
@@ -0,0 +1,68 @@
+import os
+import time
+import psycopg2
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task_group
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+
+def wait_for_postgres(
+    host=os.getenv("POSTGRES_HOST", "postgres-app"),
+    port=int(os.getenv("POSTGRES_PORT", 5432)),
+    database=os.getenv("POSTGRES_DB", "incomes_db"),
+    user=os.getenv("POSTGRES_USER", "admin"),
+    password=os.getenv("POSTGRES_PASSWORD", "admin"),
+    timeout=60,
+    interval=3,
+):
+    start_time = time.time()
+
+    while True:
+        try:
+            conn = psycopg2.connect(
+                host=host,
+                port=port,
+                dbname=database,
+                user=user,
+                password=password,
+            )
+            conn.close()
+            print("PostgreSQL disponível")
+            return
+        except Exception as e:
+            if time.time() - start_time > timeout:
+                raise Exception(f"Timeout ao conectar no PostgreSQL: {e}")
+            print("Aguardando PostgreSQL...")
+            time.sleep(interval)
+
+
+def wait_db_task():
+    return PythonOperator(
+        task_id="wait_for_postgres",
+        python_callable=wait_for_postgres,
+    )
+
+def ingestion_task():
+    return BashOperator(
+        task_id="run_ingestion",
+        bash_command="python /opt/app/src/ingestion/ingestion.py",
+    )
+
+@task_group(group_id='dbt_tasks')
+def dbt_tasks():
+    dbt_run = BashOperator(
+        task_id="run_dbt",
+        bash_command="dbt run --project-dir /opt/app/src/incomes_dbt --profiles-dir /opt/app/src/incomes_dbt",
+    )
+
+    dbt_test = BashOperator(
+        task_id="run_dbt_test",
+        bash_command="dbt test --project-dir /opt/app/src/incomes_dbt --profiles-dir /opt/app/src/incomes_dbt",
+    )
+
+    dbt_docs = BashOperator(
+        task_id="run_dbt_docs",
+        bash_command="dbt docs generate --project-dir /opt/app/src/incomes_dbt --profiles-dir /opt/app/src/incomes_dbt && echo 'DBT docs gerados'",
+    )
+    dbt_run >> dbt_test >> dbt_docs
